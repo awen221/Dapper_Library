0f561c4872b44c68ade3a2d889bbdfd403d6a6fc
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-05-26 15:55:42 +0800

    Use simple 'using' statement

--- Connection.cs
+++ Connection.cs
@@ -12,40 +12,32 @@ namespace Dapper_Library
 
         public IEnumerable<TData> Query<TData>(string sql, object? param = null, IDbTransaction? transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
         {
-            using (var connection = new TDbConnection())
-            {
-                connection.ConnectionString = ConnectionString;
+            using var connection = new TDbConnection();
+            connection.ConnectionString = ConnectionString;
 
-                return connection.Query<TData>(sql, param, transaction, buffered, commandTimeout, commandType);
-            }
+            return connection.Query<TData>(sql, param, transaction, buffered, commandTimeout, commandType);
         }
         public TData QuerySingle<TData>(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            using (var connection = new TDbConnection())
-            {
-                connection.ConnectionString = ConnectionString;
+            using var connection = new TDbConnection();
+            connection.ConnectionString = ConnectionString;
 
-                return connection.QuerySingle<TData>(sql, param, transaction, commandTimeout, commandType);
-            }
+            return connection.QuerySingle<TData>(sql, param, transaction, commandTimeout, commandType);
         }
         public TData QuerySingleOrDefault<TData>(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            using (var connection = new TDbConnection())
-            {
-                connection.ConnectionString = ConnectionString;
+            using var connection = new TDbConnection();
+            connection.ConnectionString = ConnectionString;
 
-                return connection.QuerySingleOrDefault<TData>(sql, param, transaction, commandTimeout, commandType);
-            }
+            return connection.QuerySingleOrDefault<TData>(sql, param, transaction, commandTimeout, commandType);
         }
 
         public int Execute(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            using (var connection = new TDbConnection())
-            {
-                connection.ConnectionString = ConnectionString;
+            using var connection = new TDbConnection();
+            connection.ConnectionString = ConnectionString;
 
-                return connection.Execute(sql, param, transaction, commandTimeout, commandType);
-            }
+            return connection.Execute(sql, param, transaction, commandTimeout, commandType);
         }
     }
 }
